3645da83e9a77c26b717e709a7812ac082ae0000
Ujwal Barodia <ujwalbarodia@gmail.com>
2026-05-17 00:27:57 +0530

    Test route added

--- src/routes/common.route.ts
+++ src/routes/common.route.ts
@@ -1,9 +1,15 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { CommonController } from "../controllers/common.controller";
 import { authMiddleware } from "../middlewares/auth.middleware";
 
 const commonRoutes = Router();
 
 commonRoutes.post('/getUserDetails', authMiddleware, CommonController.getUserDetails);
+commonRoutes.get('/', (req: Request, res: Response) => {
+    res.status(200).json({
+        status: 1,
+        message: "Server running successfully!"
+    })
+});
 
 export default commonRoutes;
